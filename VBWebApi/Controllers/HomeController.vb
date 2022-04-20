5d38901232dd34bdc2f578c334650a4e0a761ca5
Chris Long <longachr@amazon.com>
2022-04-20 15:03:40 -0700

    Add project files.

---
+++ VBWebApi/Controllers/HomeController.vb
@@ -0,0 +1,9 @@
+﻿Public Class HomeController
+    Inherits Microsoft.AspNetCore.Mvc.Controller
+
+    Function Index() As Microsoft.AspNetCore.Mvc.ActionResult
+        ViewData("Title") = "Home Page"
+
+        Return View()
+    End Function
+End Class
